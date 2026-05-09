3f9f002b0dacf0161b2e73712a3a3c8ecccd88d3
israel balbino <israel-balbino2012@hotmail.com>
2026-05-09 11:28:43 -0300

    ok

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,23 @@ export default function Document() {
   return (
     <Html suppressHydrationWarning lang="en">
       <Head>
-   </Head>
+
+<meta
+  name="theme-color"
+  content="#0f172a"
+/>
+
+<link
+  rel="manifest"
+  href="/manifest.json"
+/>
+
+<link
+  rel="apple-touch-icon"
+  href="/icon-192.png"
+/>
+
+</Head>
       <body className="antialiased">
         <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
         <Main />
